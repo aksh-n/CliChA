d4c3ff8b4864dc508455bbe8812015c3eeb821d9
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-11-11 11:27:48 -0500

    Add comments and reformat files

--- testscrapy/testscrapy/spiders/cbctest.py
+++ testscrapy/testscrapy/spiders/cbctest.py
@@ -1,15 +1,18 @@
+"""This is just a test file. Not intended to be used in project."""
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.http import Response
 
-# this spider now finds every link on the cbc home page
+
 class CbctestSpider(CrawlSpider):
+    """This test class finds links on the CBC homepage."""
     name = 'cbctest'
     allowed_domains = ['www.cbc.ca']
     start_urls = ['http://www.cbc.ca/']
     rules = (
-        Rule(LinkExtractor(allow='^http://www.cbc.ca/$'), callback='parse', follow=True),
+        Rule(LinkExtractor(allow='^http://www.cbc.ca/$'),
+             callback='parse', follow=True),
         Rule(LinkExtractor(), callback='parse', follow=False)
     )
 
--- testscrapy/testscrapy/spiders/nasa.py
+++ testscrapy/testscrapy/spiders/nasa.py
@@ -5,17 +5,17 @@ from scrapy.http import TextResponse
 
 class NASASpider(SitemapSpider):
     """A class used to crawl the NASA climate change site for climate change related articles."""
-    
+
     name = 'NASA'
     # only search this website for articles
     allowed_domains = ['climate.nasa.gov']
     sitemap_urls = ['https://climate.nasa.gov/sitemaps/news_items_sitemap.xml']
 
-    
     def parse(self: 'NASASpider', response: TextResponse):
-        """Parse the 
-        """     
-        txt = str.join('', response.xpath('//div[@class="wysiwyg_content"]//p/text()').getall())
+        """Parse each article to extract its text."""
+        txt = str.join('', response.xpath(
+            '//div[@class="wysiwyg_content"]//p/text()').getall()
+        )
 
         with open('nasa.txt', 'a') as f:
             f.write(txt)
