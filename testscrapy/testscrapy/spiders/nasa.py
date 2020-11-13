9f19ff626cbf62e7bfd903d058335a85696f4040
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-11-12 21:41:57 -0500

    Update nasa.py

--- testscrapy/testscrapy/spiders/nasa.py
+++ testscrapy/testscrapy/spiders/nasa.py
@@ -11,12 +11,25 @@ class NASASpider(SitemapSpider):
     allowed_domains = ['climate.nasa.gov']
     sitemap_urls = ['https://climate.nasa.gov/sitemaps/news_items_sitemap.xml']
 
+    num_of_articles = 0
+
+
+    def closed(self: "NASASpider", reason: str):
+        with open('nasa.txt', 'a') as f:
+            f.write('Articles crawled: ' + str(self.num_of_articles) + '\n')
+        
+    
     def parse(self: 'NASASpider', response: TextResponse):
         """Parse each article to extract its text."""
-        txt = str.join('', response.xpath(
-            '//div[@class="wysiwyg_content"]//p/text()').getall()
-        )
+        title = response.xpath('//h1[contains(@class, "article_title")]/text()').get().strip()
+        txtlist = response.xpath(
+            '(//div[contains(@class, "wysiwyg_content")]//p | //div[contains(@class, "wysiwyg_content")]//h3)/descendant-or-self::*/text()'
+        ).getall()
 
-        with open('nasa.txt', 'a') as f:
-            f.write(txt)
-            f.write('\n')
+        txt = str.join(' ', [s.strip() for s in txtlist])
+
+        with open('nasa.txt', 'a', encoding='utf-8') as f:
+            f.write(str(self.num_of_articles) + '->' + title + '\n' + txt)
+            f.write('\n--------\n')
+        
+        self.num_of_articles += 1
