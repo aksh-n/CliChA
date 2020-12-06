c1f098f04f173bdb11abecb44c7ddb248c180658
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-12-05 20:46:23 -0500

    Add a comment in settings

--- clicha_scrapy/clicha_scrapy/settings.py
+++ clicha_scrapy/clicha_scrapy/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'clicha_scrapy.spiders'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47'
 
 # Obey robots.txt rules
+# We are going to be unlawful programmers
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
