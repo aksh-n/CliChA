e18ee11fe6880e19d1e1438102a2d43ca3428799
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-12-10 14:08:58 -0500

    Fix variable name in nytimes documentation

--- clicha_scrapy/clicha_scrapy/spiders/nytimes.py
+++ clicha_scrapy/clicha_scrapy/spiders/nytimes.py
@@ -35,7 +35,7 @@ class NyTimesTextSpider(CrawlSpider):
         - base_url: the base url of NYTimes sitemaps
         - writers: a Dict of TextWriters that handle text formatting and output to file, each
           responsible for one year of data
-        - NUM_PER_YEAR: the maximum number of articles to scrapy for each year
+        - num_per_year: the maximum number of articles to scrapy for each year
     """
 
     name: str = 'nytimestext'
@@ -136,6 +136,9 @@ class NyTimesTextSpider(CrawlSpider):
 
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
     import python_ta
     python_ta.check_all(config={
         'allowed-io': ['*'],
