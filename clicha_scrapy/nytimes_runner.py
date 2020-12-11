03d2dea3970e9034a925ca4ed5dacd1aee6b85b8
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-12-10 20:48:49 -0500

    Add docstring to nytimes runner

--- clicha_scrapy/nytimes_runner.py
+++ clicha_scrapy/nytimes_runner.py
@@ -1,6 +1,13 @@
+"""Climate Change Awareness (CliChA), NYTimes Runner
+
+This module was used to run the NYTimesTextSpider to collect the data. It should not be imported
+or run anywhere other than as a top level script.
+
+Copyright (c) 2020 Akshat Naik and Tony Hu.
+Licensed under the MIT License. See LICENSE in the project root for license information.
+"""
 import os
 from sys import argv
-from scrapy.cmdline import execute
 
 if __name__ == '__main__':
     start = int(argv[1])
@@ -8,4 +15,6 @@ if __name__ == '__main__':
 
     for i in range(start, end, 5):
         print('Starting job ' + str(i))
-        os.system(f'scrapy crawl nytimestext -a start={i} -a end={i+4} --logfile ".\\nytimeslog\\{i}.txt"')
+        os.system(
+            f'scrapy crawl nytimestext -a start={i} -a end={i+4} --logfile ".\\nytimeslog\\{i}.txt"'
+        )
