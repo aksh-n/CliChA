d406e137d3915ac6682502ca0f96b3c15773c673
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-12-13 11:37:49 -0500

    Add documentation to main_backend

--- main_backend.py
+++ main_backend.py
@@ -1,13 +1,24 @@
+"""Climate Change Awareness (CliChA), Main Backend
+
+This module contains functions that do a demo of scrapying and processing,
+to be presented in the GUI program.
+
+Copyright (c) 2020 Akshat Naik and Tony Hu.
+Licensed under the MIT License. See LICENSE in the project root for license information.
+"""
 from clicha_scrapy import demo_nytimes
 from find_climate_articles import article_climate_awareness_index, create_idf_dict
 import spaCy_helpers as sh
 
+
 def run_demo_nytimes() -> None:
+    """Call the demo version of the NyTimesTextSpider."""
     demo_nytimes.run_spider()
 
+
 def demo_processing_cai() -> list:
-    """Returns a list of floats, each corresponding to a Climate Awareness Index of an article.
-    
+    """Return a list of floats, each corresponding to a Climate Awareness Index of an article.
+
     This is a demo processing adapted from find_climate_articles.py but in much smaller scale.
     WARNING: Run ONLY after run_demo_nytimes()
     """
@@ -23,5 +34,29 @@ def demo_processing_cai() -> list:
         list_articles_cai.append(article_cai)
     return list_articles_cai
 
+
 if __name__ == '__main__':
-    pass
+    import doctest
+    doctest.testmod()
+
+    import python_ta
+    python_ta.check_all(config={
+        'allowed-io': ['demo_processing_cai'],
+        'extra-imports': [
+            'clicha_scrapy',
+            'find_climate_articles',
+            'spaCy_helpers',
+            'python_ta.contracts'
+        ],
+        'max-line-length': 100,
+        'max-args': 6,
+        'max-locals': 25,
+        # W0221: by the documentation, parse does not need **kwargs
+        # W0613: 'closed' is called by Scrapy, requiring the 'reason' argument
+        'disable': ['R1705', 'W0221', 'W0613'],
+    })
+
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
