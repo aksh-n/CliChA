93e5d839db97b93f86f17f311026789d2133b0af
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-12-13 11:40:40 -0500

    Remove whitespace in main.py

--- main.py
+++ main.py
@@ -15,21 +15,23 @@ from main_backend import run_demo_nytimes, demo_processing_cai
 # Importing matplotlib and required classes and functions to integrate graphs into tkinter GUI.
 import matplotlib
 from matplotlib import pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 matplotlib.use("TkAgg")
 
 
 WIDTH = 1250
-HEIGHT= 800
+HEIGHT = 800
 HEIGHT_WIDGET = 30
-MENU = [ 
+MENU = [
     '1. miniature scraping and processing + show graph',
     '2. A "Climate Awareness Index (CAI) vs Years" line graph for New York Times and Science Daily',
     '3. A "Number of Climate Aware Articles vs Years" line graph for New York Times and Science Daily',
     '4. A Comparison of Nytimes and Science Daily 1998-2020',
     '5. A Comparison of Climate Awareness Index of Nytimes/Science Daily and CO2 concentration'
 ]
+
 
 def create_app():
     """Creates the Application."""
@@ -51,7 +53,7 @@ def config_plot():
 class Application(tk.Frame):
     def __init__(self, master=None):
         """Initializes the main frame of the application.
-        
+
         Instance Attributes:
             - master: an instance of tk.Tk
         """
@@ -61,30 +63,31 @@ class Application(tk.Frame):
         self.place(x=0, y=0, w=WIDTH, h=HEIGHT)
         self.create_widgets()
         self.graph_drawn = False
-    
 
     def create_widgets(self):
         """Creates all the starting widgets and places them in their respective places."""
-        self.Frame1 = tk.Frame(self.master, bg = "pink")
-        self.Frame1.place(x = 0, y = 0, height = HEIGHT_WIDGET * (len(MENU) + 2), width = WIDTH)
+        self.Frame1 = tk.Frame(self.master, bg="pink")
+        self.Frame1.place(x=0, y=0, height=HEIGHT_WIDGET *
+                          (len(MENU) + 2), width=WIDTH)
         self.create_menu_and_buttons()
-        self.demo_message = tk.Label(self.Frame1, text = "", fg="white", bg="MediumPurple4")
+        self.demo_message = tk.Label(
+            self.Frame1, text="", fg="white", bg="MediumPurple4")
         self.demo_message.place(
-            x = 0, y = (len(MENU) + 1) * HEIGHT_WIDGET, height = HEIGHT_WIDGET, width = WIDTH
+            x=0, y=(len(MENU) + 1) * HEIGHT_WIDGET, height=HEIGHT_WIDGET, width=WIDTH
         )
-        self.Frame2 = tk.Frame(self.master, bg = "pink1")
+        self.Frame2 = tk.Frame(self.master, bg="pink1")
         self.Frame2.place(
-            x = 0, y = HEIGHT_WIDGET * (len(MENU) + 2),
-            height = HEIGHT - HEIGHT_WIDGET * (len(MENU) + 2), width = WIDTH
+            x=0, y=HEIGHT_WIDGET * (len(MENU) + 2),
+            height=HEIGHT - HEIGHT_WIDGET * (len(MENU) + 2), width=WIDTH
         )
 
     def create_menu_and_buttons(self):
         """Creates and places the menu and buttons."""
         self.intro = tk.Label(
-            self.Frame1, text = "WELCOME! Here are a few options:", fg="white", bg="black",
+            self.Frame1, text="WELCOME! Here are a few options:", fg="white", bg="black",
             anchor="w", justify="left"
         )
-        self.intro.place(x = 0, y = 0, height=HEIGHT_WIDGET, width=WIDTH)
+        self.intro.place(x=0, y=0, height=HEIGHT_WIDGET, width=WIDTH)
         self.intro.config(font=("Courier", 20))
 
         self.options = [None] * len(MENU)
@@ -99,15 +102,17 @@ class Application(tk.Frame):
                 self.Frame1, text=MENU[i], fg="white", bg="VioletRed3", anchor='w', justify='left'
             )
             self.options[i].place(
-                x = 50, y = (i + 1) * HEIGHT_WIDGET, height=HEIGHT_WIDGET, width=WIDTH
+                x=50, y=(i + 1) * HEIGHT_WIDGET, height=HEIGHT_WIDGET, width=WIDTH
             )
-            self.buttons[i] = tk.Button(self.Frame1, text=f"B{i + 1}", fg="white", bg="black")
+            self.buttons[i] = tk.Button(
+                self.Frame1, text=f"B{i + 1}", fg="white", bg="black")
             self.buttons[i].place(
-                x = 0, y = (i + 1) * HEIGHT_WIDGET, height=HEIGHT_WIDGET, width=50
+                x=0, y=(i + 1) * HEIGHT_WIDGET, height=HEIGHT_WIDGET, width=50
             )
             self.options[i].config(font=("Courier", 15))
-            self.buttons[i].config(font=("Courier", 15), command=show_graphs[i])
-    
+            self.buttons[i].config(font=("Courier", 15),
+                                   command=show_graphs[i])
+
     def draw_graph(self):
         """Initializes and draws an empty graph to draw on."""
         self.graph_drawn = True
@@ -115,7 +120,7 @@ class Application(tk.Frame):
         self.graphIndex = 0
         self.canvas = FigureCanvasTkAgg(self.fig, self.Frame2)
         self.config_window()
-    
+
     def config_window(self):
         """Configures the window to add Switch Graph Button Quit Buttons and a Toolbar."""
         toolbar = NavigationToolbar2Tk(self.canvas, self.Frame2)
@@ -123,7 +128,8 @@ class Application(tk.Frame):
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         self.button = tk.Button(self.Frame2, text="Quit", command=self._quit)
         self.button.pack(side=tk.BOTTOM)
-        self.button_switch = tk.Button(self.Frame2, text="Switch Graphs") #, command=self.switch_graphs)
+        # , command=self.switch_graphs)
+        self.button_switch = tk.Button(self.Frame2, text="Switch Graphs")
         self.button_switch.pack(side=tk.BOTTOM)
 
     def demo_start(self):
@@ -134,7 +140,7 @@ class Application(tk.Frame):
     #     self.ax.clear() # clear current axes
     #     self.button_switch["text"] = "Start Demo!"
     #     self.button_switch["command"] = threading.Thread(target=self.demo_scraping_processing_graph)
-    
+
     # def demo_scraping_processing_graph(self):
     #     self.demo_message["text"] = "Now Scraping, Please Wait."
     #     threading.Thread(target=run_demo_nytimes()).start()
@@ -142,7 +148,7 @@ class Application(tk.Frame):
     #     threading.Thread(target=demo_graph(self.ax, demo_processing_cai())).start()
     #     self.demo_message["text"] = ""
     #     print("Done!")
-    
+
     def nytimes_cai(self):
         """Draws and displays the Climate Awareness Index (CAI) graph for New York Times."""
         if not self.graph_drawn:
@@ -152,7 +158,7 @@ class Application(tk.Frame):
         self.button_switch["command"] = self.science_daily_cai
         show_line_graph(self.ax, 'nytimes')
         self.canvas.draw()
-    
+
     def science_daily_cai(self):
         """Draws and displays the Climate Awareness Index (CAI) graph for Science Daily."""
         if not self.graph_drawn:
@@ -162,7 +168,7 @@ class Application(tk.Frame):
         self.button_switch["command"] = self.nytimes_cai
         show_line_graph(self.ax, 'science_daily_small')
         self.canvas.draw()
-    
+
     def nytimes_num_articles(self):
         """Draws and displays the Number of Climate Aware Articles graph for New York Times."""
         if not self.graph_drawn:
@@ -172,7 +178,7 @@ class Application(tk.Frame):
         self.button_switch["command"] = self.science_daily_num_articles
         show_line_graph(self.ax, 'nytimes', False)
         self.canvas.draw()
-    
+
     def science_daily_num_articles(self):
         """Draws and displays the Climate Awareness Index (CAI) graph for Science Daily."""
         if not self.graph_drawn:
@@ -182,7 +188,7 @@ class Application(tk.Frame):
         self.button_switch["command"] = self.nytimes_num_articles
         show_line_graph(self.ax, 'science_daily_small', False)
         self.canvas.draw()
-    
+
     def comparison_cai(self):
         """Draws and displays the Comparison of Climate Awareness Index (CAI) graph
         for New York Times and Science Daily."""
@@ -193,7 +199,7 @@ class Application(tk.Frame):
         self.button_switch["command"] = self.comparison_num_articles
         show_bar_graph(self.ax, 'science_daily_small', 'nytimes')
         self.canvas.draw()
-    
+
     def comparison_num_articles(self):
         """Draws and displays the Comparison of Number of Climate Aware Articles graph
         for New York Times and Science Daily."""
@@ -216,7 +222,7 @@ class Application(tk.Frame):
         self.ax2 = self.ax.twinx()
         co2_comparison_graph(self.ax, self.ax2, 'nytimes')
         self.canvas.draw()
-    
+
     def co2_comparison_science_daily(self):
         """Draws and displays the Comparison of Climate Awareness Index (CAI) graph
         for Science Daily and the CO2 concentrations in the World."""
@@ -233,7 +239,7 @@ class Application(tk.Frame):
         """Clears the current figure."""
         self.fig.clear()
         self.ax = self.fig.add_subplot(111)
-    
+
     def _quit(self):
         """Quits the application."""
         self.Frame2.quit()
