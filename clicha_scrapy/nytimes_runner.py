626b938d3edf85fa968e05c7256d70c6a51d8328
Tony (Juntao) Hu <807769469hjt@gmail.com>
2020-12-13 11:09:25 -0500

    Add documentation to nytimes_runner.py

--- clicha_scrapy/nytimes_runner.py
+++ clicha_scrapy/nytimes_runner.py
@@ -10,6 +10,43 @@ import os
 from sys import argv
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    import python_ta
+    python_ta.check_all(config={
+        'allowed-io': ['*'],
+        'extra-imports': ['scrapy',
+                          'scrapy.spiders',
+                          'scrapy.http',
+                          'scrapy.crawler',
+                          'scrapy.utils.project',
+                          'scrapy.exceptions',
+                          'text_writer',
+                          'clicha_scrapy.text_writer',
+                          'clicha_scrapy.clicha_scrapy.spiders.nytimes',
+                          'random',
+                          'typing',
+                          'os',
+                          'sys',
+                          'inspect',
+                          'python_ta.contracts'],
+        'max-line-length': 99,
+        'max-args': 5,
+        'max-locals': 24,
+        # W0220: by the documentation, parse does not need **kwargs
+        # W0612: 'closed' is called by Scrapy, requiring the 'reason' argument
+        'disable': ['R1704', 'W0221', 'W0613'],
+    })
+
+    import python_ta.contracts
+
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+
+    # -----------------------------------------------------------
+    # the actual code
+
     start = int(argv[1])
     end = int(argv[2])
 
